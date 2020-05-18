31e27ccf8f8176dee5e30f88e0d2f53a343ab3ef
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-05-18 14:15:31 +0700

    InsertionSort

--- src/Main.java
+++ src/Main.java
@@ -18,9 +18,22 @@ public class Main {
         }
         System.out.println (Arrays.toString (array) +  "selectSort" );
     }
+    public static void insertionSort(int[] arr){
+        for  ( int  i =  1 ; i <arr.length; i ++) {
+            int  temp = arr [i];
+            int  j = i -  1 ;
+            for  (; j >=  0  && arr [j]> temp; j--) {
+                // Moves the value greater than temp back by one unit
+                arr [j +  1 ] = arr [j];
+            }
+            arr [j +  1 ] = temp;
+        }
+        System.out.println (Arrays.toString (arr) +  "insertSort" );
+    }
 
     public static void main(String[] args) {
         int[] arr = {5,1,12,-5,16};
         selectSort(arr);
+        insertionSort(arr);
     }
 }
